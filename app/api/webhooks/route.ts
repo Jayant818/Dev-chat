038ea796e08f7365f42a1ff5219343f78f9b58ee
Jayant <jayant@errsole.com>
2024-01-17 14:07:22 +0530

    fix es-lint errors.....

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -2,7 +2,6 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
-import { create } from "domain";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
