ff6f219154fbb64b18ee0b940a34033fa1c637dc
Jayant <jayant@errsole.com>
2024-01-21 01:36:53 +0530

    .......

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
-import { SignInButton, SignUpButton, SignedOut, useAuth } from "@clerk/nextjs";
+import { SignedOut, useAuth } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
 
@@ -61,7 +61,7 @@ const LeftSidebar = () => {
 
 			<SignedOut>
 				<div className="flex flex-col gap-3">
-					<SignInButton>
+					<Link href="/sign-in">
 						<Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
 							<Image
 								src="/assets/icons/account.svg"
@@ -74,9 +74,9 @@ const LeftSidebar = () => {
 								Log In
 							</span>
 						</Button>
-					</SignInButton>
+					</Link>
 
-					<SignUpButton mode="modal">
+					<Link href="/sign-up">
 						<Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
 							<Image
 								src="/assets/icons/sign-up.svg"
@@ -87,7 +87,7 @@ const LeftSidebar = () => {
 							/>
 							<span className="max-lg:hidden">Sign Up</span>
 						</Button>
-					</SignUpButton>
+					</Link>
 				</div>
 			</SignedOut>
 		</section>
@@ -95,3 +95,101 @@ const LeftSidebar = () => {
 };
 
 export default LeftSidebar;
+
+// "use client";
+
+// import Link from "next/link";
+// import Image from "next/image";
+// import { usePathname } from "next/navigation";
+
+// import { SignInButton, SignUpButton, SignedOut, useAuth } from "@clerk/nextjs";
+
+// import { Button } from "@/components/ui/button";
+
+// import { sidebarLinks } from "@/constants";
+
+// const LeftSidebar = () => {
+// 	const { userId } = useAuth();
+// 	const pathname = usePathname();
+
+// 	return (
+// 		<section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+// 			<div className="flex flex-1 flex-col gap-6">
+// 				{sidebarLinks.map((link) => {
+// 					const isActive =
+// 						(pathname.includes(link.route) && link.route.length > 1) ||
+// 						pathname === link.route;
+
+// 					if (link.route === "/profile") {
+// 						if (userId) {
+// 							link.route = `${link.route}/${userId}`;
+// 						} else {
+// 							return null;
+// 						}
+// 					}
+
+// 					return (
+// 						<Link
+// 							key={link.route}
+// 							href={link.route}
+// 							className={`${
+// 								isActive
+// 									? "primary-gradient rounded-lg text-light-900"
+// 									: "text-dark300_light900"
+// 							} flex items-center justify-start gap-4 bg-transparent p-4`}
+// 						>
+// 							<Image
+// 								src={link.imgURL}
+// 								alt={link.label}
+// 								width={20}
+// 								height={20}
+// 								className={`${isActive ? "" : "invert-colors"}`}
+// 							/>
+// 							<p
+// 								className={`${
+// 									isActive ? "base-bold" : "base-medium"
+// 								} max-lg:hidden`}
+// 							>
+// 								{link.label}
+// 							</p>
+// 						</Link>
+// 					);
+// 				})}
+// 			</div>
+
+// 			<SignedOut>
+// 				<div className="flex flex-col gap-3">
+// 					<SignInButton>
+// 						<Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+// 							<Image
+// 								src="/assets/icons/account.svg"
+// 								alt="sign in"
+// 								width={20}
+// 								height={20}
+// 								className="invert-colors lg:hidden"
+// 							/>
+// 							<span className="primary-text-gradient max-lg:hidden">
+// 								Log In
+// 							</span>
+// 						</Button>
+// 					</SignInButton>
+
+// 					<SignUpButton mode="modal">
+// 						<Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+// 							<Image
+// 								src="/assets/icons/sign-up.svg"
+// 								alt="sign up"
+// 								width={20}
+// 								height={20}
+// 								className="invert-colors lg:hidden"
+// 							/>
+// 							<span className="max-lg:hidden">Sign Up</span>
+// 						</Button>
+// 					</SignUpButton>
+// 				</div>
+// 			</SignedOut>
+// 		</section>
+// 	);
+// };
+
+// export default LeftSidebar;
